0774009a861067a729d0c0350a2061e271dd895c
RamaPramidha <devi.bajana@gmail.com>
2023-01-11 10:32:44 +0530

    commited and sent

--- src/test/java/programs/Class1.java
+++ src/test/java/programs/Class1.java
@@ -4,11 +4,14 @@ import org.testng.annotations.Test;
 
 public class Class1 {
 	
+	
+	
 	@Test
 	public void createUser()
 	{
 		System.out.println("User Created");
 		System.out.println("user has been created");
+		System.out.println("User");
 	}
 
 }
