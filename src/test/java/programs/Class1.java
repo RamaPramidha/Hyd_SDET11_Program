db689e87d1d2eb0ec7df13243f31bf6730be5236
RamaPramidha <devi.bajana@gmail.com>
2023-01-11 10:40:22 +0530

    commited new changes

--- src/test/java/programs/Class1.java
+++ src/test/java/programs/Class1.java
@@ -9,9 +9,11 @@ public class Class1 {
 	@Test
 	public void createUser()
 	{
-		System.out.println("User Created");
+		System.out.println("User Created"); 
 		System.out.println("user has been created");
 		System.out.println("User");
+		
+		
 	}
 
 }
